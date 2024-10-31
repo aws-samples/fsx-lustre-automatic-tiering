81a6903a14b608c816e3bebb596b1a2ec885069c
Vikrant Telkar <telkarv@amazon.com>
2024-10-31 10:29:50 -0500

    Create Single NAT Gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,13 +36,16 @@ data "aws_availability_zones" "available" {
 
 module "vpc" {
   # source             = "terraform-aws-modules/vpc/aws"
-  source          = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v5.13.0"
-  name            = "demo-vpc"
-  cidr            = "10.0.0.0/16"
-  azs             = data.aws_availability_zones.available.names
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  tags            = local.tags
+  source                 = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v5.13.0"
+  name                   = "demo-vpc"
+  cidr                   = "10.0.0.0/16"
+  azs                    = data.aws_availability_zones.available.names
+  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets         = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+  tags                   = local.tags
 }
 
 
